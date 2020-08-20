c73efa733acdc5ebd8957b39f978bf4b6bb8c6bd
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-20 09:28:24 -0700

    improve UI with dragleave event

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,6 +199,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;"); //adding CSS style when dragging
     }
 };
 
@@ -218,8 +219,17 @@ var dropTaskHandler = function(event) {
     else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+
+    dropZoneEl.removeAttribute("style"); //removes CSS styling right before drop
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+    taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
@@ -230,3 +240,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
